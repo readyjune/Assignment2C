34d9fd07228c460cc670b67572697ac936921247
jinwoo <20874214@student.curtin.edu.au>
2023-09-29 10:56:43 +0800

    Client and ClientServer made

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string uploadedPythonFilePath;
+        private string uploadedPythonFileName;
+        private string providedIPAddress;
+        private int providedPort;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BrowsePythonCodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Python Files|*.py|All Files|*.*";
+            openFileDialog.DefaultExt = ".py";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    uploadedPythonFilePath = openFileDialog.FileName;  // Save the file path
+                    uploadedPythonFileName = System.IO.Path.GetFileName(uploadedPythonFilePath);
+                    var pythonCodeContent = File.ReadAllText(uploadedPythonFilePath);
+                    PythonCodeInput.Text = pythonCodeContent;  // Set the TextBox to the actual Python code
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message);
+                }
+            }
+        }
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (TcpClient client = new TcpClient("127.0.0.1", 12345))  // Connect to the server
+                {
+                    NetworkStream stream = client.GetStream();
+                    StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+                    StreamReader reader = new StreamReader(stream);
+
+                    // Start the communication
+                    writer.WriteLine("Upload");
+                    writer.WriteLine(uploadedPythonFileName);
+                    writer.WriteLine(PythonCodeInput.Text);
+
+                    // Read the response from the server
+                    string response = reader.ReadLine();
+                    MessageBox.Show(response);  // Show the server's response
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while uploading the file: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ ClientServer/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 12345;  // You can choose any available port
+            TcpListener server = null;
+
+            try
+            {
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");  // Listen on localhost
+                server = new TcpListener(localAddr, port);
+                server.Start();
+
+                Console.WriteLine("Server started...");
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    NetworkStream stream = client.GetStream();
+                    StreamReader reader = new StreamReader(stream);
+                    StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+
+                    string message = reader.ReadLine();
+
+                    switch (message)
+                    {
+                        case "Hello":
+                            writer.WriteLine("Hello Client!");
+                            break;
+
+                        case "Upload":
+                            string fileName = reader.ReadLine();
+                            string fileContent = reader.ReadLine();
+                            File.WriteAllText($"./{fileName}", fileContent);
+                            writer.WriteLine("File uploaded successfully!");
+
+                            // Print the uploaded filename and its content
+                            Console.WriteLine($"Received file: {fileName}");
+                            Console.WriteLine("File Content:");
+                            Console.WriteLine("--------------");
+                            Console.WriteLine(fileContent);
+                            Console.WriteLine("--------------");
+                            break;
+
+                        case "Goodbye":
+                            writer.WriteLine("Goodbye Client!");
+                            break;
+
+                        default:
+                            writer.WriteLine("Unknown command!");
+                            break;
+                    }
+
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"SocketException: {e}");
+            }
+            finally
+            {
+                server?.Stop();
+            }
+        }
+    }
+}
