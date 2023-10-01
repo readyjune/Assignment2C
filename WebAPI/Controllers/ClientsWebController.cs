4e2cf7e39d250309c941f2491c1cf4f3597f7028
Tui_Warihana <tui.warihana@student.curtin.edu.au>
2023-10-01 20:48:36 +0800

    Database is showing on website.

---
+++ WebAPI/Controllers/ClientsWebController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+
+namespace WebAPI.Controllers
+{
+    public class ClientsWebController : Controller
+    {
+        private readonly ClientContext _context;
+
+        public ClientsWebController(ClientContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var clients = await _context.Clients.ToListAsync();
+            return View("ClientsInfo", clients);  // Render the Razor view with the client data
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -31,6 +31,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=ClientsWeb}/{action=Index}/{id?}");
+
 
 app.Run();
